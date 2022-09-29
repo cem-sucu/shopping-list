9ac2732a3227e72fe5d0fbd5c0e0cbdaee27777a
cem-sucu <cems.cems.17@gmail.com>
2022-09-29 14:42:28 +0200

    indiquez l'article, validez et l'afficher

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    FlatList,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>nouveau projet shopping list</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [product, setProduct] = useState("");
+    const [myProducts, setMyProducts] = useState([]);
+
+    const inputHandler = (val) => {
+        setProduct(val);
+    };
+
+    const submitHandler = () => {
+        const idString = Date.now().toString();
+        setMyProducts((currentMyProduct) => [
+            { key: idString, name: product },
+            ...currentMyProduct,
+        ]);
+        setProduct("");
+    };
+    return (
+        // view de l'input
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Nouveau produit"
+                    onChangeText={inputHandler}
+                    value={product}
+                />
+                <Button title="valider" onPress={submitHandler} />
+            </View>
+
+            {/* view des items validé et affiché */}
+            <FlatList
+                data={myProducts}
+                renderItem={({ item }) => (
+                    <Text style={styles.element}>{item.name}</Text>
+                )}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: { padding: 40, paddingTop: 60 },
+    inputContainer: { flexDirection: "row", marginBottom: 15 },
+    textInput: {
+        borderColor: "grey",
+        borderWidth: 1,
+        padding: 5,
+        paddingLeft: 9,
+        fontSize: 18,
+        flexGrow: 1,
+    },
+    items: { marginTop: 10 },
+    element: {
+        backgroundColor: "#87cefa",
+        borderRadius: 5,
+        padding: 20,
+        fontSize: 17,
+        marginVertical: 6,
+    },
 });
