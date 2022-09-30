9a21f646bc2756145581178f0988d6179892e6cd
cem-sucu <cems.cems.17@gmail.com>
2022-09-30 10:02:28 +0200

    TouchableHighlight

--- components/Products.js
+++ components/Products.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
 export const Products = ({ name }) => {
     return (
-        <TouchableOpacity
+        <TouchableHighlight
             style={{ backgroundColor: "cyan" }}
             onPress={() => console.log("supprimé")}
             activeOpacity={0.8} // gere la couleur du toucher lorsque l'on appuie sur le l'élèment
+            underlayColor="darkblue" // permet de changer la couleur au moment du press sur l'élèment
         >
-            <View style={styles.items} onPress={() => console.log("supprimé")}>
+            <View style={styles.items}>
                 <Text style={styles.element}>{name}</Text>
             </View>
-        </TouchableOpacity>
+        </TouchableHighlight>
     );
 };
 
